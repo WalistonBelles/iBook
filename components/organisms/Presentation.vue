c6010fd0b7b1273f32a06551bd188d436fd19469
Waliston Belles <walistonbelles1@gmail.com>
2021-11-20 13:55:16 -0300

    Start Project

---
+++ components/organisms/Presentation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="presentation">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis vitae ea voluptatum maiores ex qui, eligendi atque molestias tenetur itaque perspiciatis quos exercitationem, obcaecati dolorem, quasi sit repudiandae iure nemo.
+    </p>
+
+    <img src="@/assets/img/presentation.svg" alt="Apresentação do iBook">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.presentation {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 2rem;
+  align-items: center;
+  @include screen('small', 'medium') {
+    grid-template-columns: 1fr;
+  }
+  p {
+    line-height: 26px;
+  }
+  img {
+    max-width: 100%;
+  }
+}
+</style>
